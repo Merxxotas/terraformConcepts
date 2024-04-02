eb33b8f9b725f4b81a68ac54d1eaeb72aa1286b1
Brayan Marin Guirales <brayan6590@gmail.com>
2024-04-02 17:01:05 -0500

    doing an alternative with the -EOF copying to a .json file, adding a new resource on the main.tf (s3_bucket and s3_object)

--- 3-AWS_introduction/main.tf
+++ 3-AWS_introduction/main.tf
@@ -24,21 +24,31 @@ resource "aws_iam_user" "admin-user" {
 
 resource "aws_iam_policy" "admin-user" {
   name   = "AdminUsers"
-  policy = <<-EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "*",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = file("admin-policy.json")
 }
 
 resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
   user       = aws_iam_user.admin-user.name
   policy_arn = aws_iam_policy.admin-user.arn
+}
+
+resource "aws_s3_bucket" "finance" {
+  bucket = "finanace-02042024"
+  tags = {
+    description = "Finance and Payroll"
+  }
+}
+
+
+# resource "aws_s3_bucket_object" "finance-2024" {
+#   content = "/home/brayanmarin/s3Proof/terraformVideo0.mp4"
+#   key     = "terraformVideo0.mp4"
+#   bucket  = aws_s3_bucket.finance.id
+# }
+
+
+resource "aws_s3_object" "finance-2024" {
+  source = "/home/brayanmarin/s3Proof/terraformVideo0.mp4"
+  key    = "terraformVideo0.mp4"
+  bucket = aws_s3_bucket.finance.id
 }
